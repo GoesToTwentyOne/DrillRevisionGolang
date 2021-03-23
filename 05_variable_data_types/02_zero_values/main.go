a070d9dd34a4cbf0bac852fac1b698e852c9fecf
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-23 16:47:43 +0600

    zero values

---
+++ 05_variable_data_types/02_zero_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b bool
+	var c string
+	var d float32
+	var e rune
+	//zerro values
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+}
