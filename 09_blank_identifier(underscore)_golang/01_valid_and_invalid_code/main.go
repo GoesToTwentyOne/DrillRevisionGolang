fce6f5d03dcb2ac16b181ed6d6d9871cd6db3674
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-23 21:17:30 +0600

    underscore blank identifier

---
+++ 09_blank_identifier(underscore)_golang/01_valid_and_invalid_code/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+	fmt.Println(a)
+	fmt.Println(b)
+	//golang has garbage collected language if you declare a variable but not used in program the program
+	// didn't execute the program
+
+}
