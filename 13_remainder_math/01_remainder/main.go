a37809747f605d989b0c1fd94065155999600e11
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-04 19:56:32 +0600

    remainder math

---
+++ 13_remainder_math/01_remainder/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// In mathematics, the remainder is the amount "left over" after performing some computation. In arithmetic,
+	// the remainder is the integer "left over"
+	// after dividing one integer by another to produce an integer quotient (integer division)
+	x := 13 % 2
+	fmt.Println(x)
+
+}
