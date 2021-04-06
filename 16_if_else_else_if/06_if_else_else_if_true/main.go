c36e1f9a296d2715cb612c97e9921b837d565c31
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-06 19:46:02 +0600

    if else else if true

---
+++ 16_if_else_else_if/06_if_else_else_if_true/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	if false {
+		fmt.Println("This is Avangers")
+	} else if true {
+		fmt.Println("This is Thriller")
+	} else {
+		fmt.Println("This is Comedy")
+	}
+}
