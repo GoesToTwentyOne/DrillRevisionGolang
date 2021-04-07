c4054770025199c74186807f46e7c2615b35289a
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-07 21:41:50 +0600

    variadic func parameters

--- 19_function/07_variadic_func/main.go
+++ 19_function/07_variadic_func/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	avg(5, 10, 20)
+	//variadic function check
+	fmt.Println(avarage(1, 2, 3, 4, 5, 10, 25, 8, 7, 7.8, 4.72))
 
 }
 func avg(n ...float64) {
 	fmt.Println(n)
 	fmt.Printf("%T \n", n) //float64 array type
 
-	//variadic function check
-	fmt.Println(avarage(1, 2, 3, 4, 5, 10, 25, 8, 7, 7.8, 4.72))
 }
 func avarage(n ...float64) float64 {
 	var sum float64 = 0
