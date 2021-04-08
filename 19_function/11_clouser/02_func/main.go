2e0bf7bba0c3e8c57a3685b38d21231696b1b398
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-08 19:49:55 +0600

    func

--- 19_function/11_clouser/02_func/main.go
+++ 19_function/11_clouser/02_func/main.go
@@ -14,3 +14,8 @@ func inc() int {
 	x++
 	return x
 }
+
+//function in  function called clouser function
+// closure helps us limit the scope of variables used by multiple functions
+// without closure, for two or more funcs to have access to the same variable,
+// that variable would need to be package scope
