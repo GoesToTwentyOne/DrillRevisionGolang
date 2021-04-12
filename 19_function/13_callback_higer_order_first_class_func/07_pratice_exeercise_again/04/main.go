4489824a6b25640d564864e7127f3644b4f67a9d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-12 20:50:05 +0600

    osam

---
+++ 19_function/13_callback_higer_order_first_class_func/07_pratice_exeercise_again/04/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	goes(func(x []int) {
+		fmt.Println(x)
+	}, []int{1, 2, 3, 4, 5})
+
+}
+func goes(callback func(x []int), numbers []int) {
+	var xs []int
+
+	for _, value := range numbers {
+		xs = append(xs, value)
+
+	}
+	callback(xs)
+}
