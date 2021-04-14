445547fe284ec5928d16ce2f2c2e9c85a3e6f071
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-14 20:14:16 +0600

    indexing access

---
+++ 22_slice/06_slicing_a_slicing/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x []int
+	fmt.Println(x)
+	y := []string{"a", "b", "c", "d", "e"}
+	fmt.Println(y)
+	fmt.Println(y[:2])
+	fmt.Println(y[3])
+	fmt.Println("Nihad"[1])
+
+}
