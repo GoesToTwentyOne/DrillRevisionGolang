adb44293592a1d32853618c1de915cf4544be74d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-14 14:10:00 +0600

    array to string conv

--- 21_array/02_array_ascii_to_alphabet/main.go
+++ 21_array/02_array_ascii_to_alphabet/main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var smallAlphabet [26]string
 	for i := 97; i <= 122; i++ {
-		smallAlphabet[i-97] = string(i)
+		smallAlphabet[i-97] = string(rune(i))
 	}
 	fmt.Println(smallAlphabet)
 	fmt.Println(smallAlphabet[25])
