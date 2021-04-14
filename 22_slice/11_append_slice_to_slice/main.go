b8e616aa06624deab117a53a5afb6f2e7d2d8219
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-14 21:09:08 +0600

    append slice of a slice

---
+++ 22_slice/11_append_slice_to_slice/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	y := []int{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	x = append(x, y...)
+	fmt.Println(x)
+}
