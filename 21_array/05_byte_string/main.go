05ec634bc7b0b9d784be9bb492829ee3929a2d7d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-14 15:25:56 +0600

    byte string

---
+++ 21_array/05_byte_string/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var x [256]byte
+	// for i := 0; i < 256; i++ {
+	// 	x[i] = byte(i)
+	// }
+	// // fmt.Println(x)
+	// for i, value := range x {
+	// 	fmt.Printf("%v - %T - %b\n", value, value, value)
+	// 	if i > 50 {
+	// 		break
+	// 	}
+
+	// }
+	var x [256]string
+
+	fmt.Println(len(x))
+	fmt.Println(x[0])
+	for i := 0; i < 256; i++ {
+		x[i] = string(rune(i))
+	}
+	// fmt.Println(x)
+	for _, v := range x {
+		fmt.Printf("%v - %T - %v\n", v, v, []byte(v))
+	}
+
+}
