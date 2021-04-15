d081fd057a2f21805e3439218deba17b9661c67d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-15 13:56:11 +0600

    shotrhand literal entrance

---
+++ 23_map/06_shorthand_literal_entry/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	myMap := map[string]string{
+		"A": "Apple",
+		"B": "Bat",
+		"C": "Cat",
+		"D": "Dog",
+		"E": "Egg",
+	}
+	myMap["F"] = "Fan"
+	myMap["G"] = "Gan"
+	fmt.Println(myMap)
+	fmt.Println(myMap["G"])
+
+}
