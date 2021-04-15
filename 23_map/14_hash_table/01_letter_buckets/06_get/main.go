2acac6fd92f1c66eeae8136c3c0b16b23ea5e0e4
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-15 21:38:52 +0600

    net package get

---
+++ 23_map/14_hash_table/01_letter_buckets/06_get/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://golang.org/pkg/math/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", bs)
+}
