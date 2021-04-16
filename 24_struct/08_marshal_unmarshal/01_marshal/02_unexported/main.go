d131670a10fc773850fbe4b691fda13225aef49c
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-16 14:34:07 +0600

    unexported

---
+++ 24_struct/08_marshal_unmarshal/01_marshal/02_unexported/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	fname      string
+	lname      string
+	age        int
+	unexported bool
+}
+
+func main() {
+	p := person{
+		fname:      "Nihad",
+		lname:      "Hossain",
+		age:        21,
+		unexported: true,
+	}
+	// p2 := person{
+	// 	Fname:      "Neha",
+	// 	Lname:      "Jannat",
+	// 	Age:        06,
+	// 	unexported: true,
+	// }
+	bs, err := json.Marshal(p)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(string(bs))
+
+}
