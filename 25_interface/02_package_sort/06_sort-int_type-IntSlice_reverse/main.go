c07ba438654e39ec9910c75a0739e946bbcc02e0
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-18 06:32:28 +0600

    sort

---
+++ 25_interface/02_package_sort/06_sort-int_type-IntSlice_reverse/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := []int{1, 5, 78, 9, 45, 12, 54, 8, 6, 3, 9, 8, 7, 1, 4, 5, 4}
+	sort.Sort(sort.Reverse(sort.IntSlice(s)))
+	fmt.Println(s)
+
+}
