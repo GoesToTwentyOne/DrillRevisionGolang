4232bfed6e271acce0e2768384c57a46ae0a0d30
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-19 07:14:20 +0600

    string to int conv

--- 25_interface/05_conversion_and_assertions/01_conversion/06_strconv/01_Atoi/main.go
+++ 25_interface/05_conversion_and_assertions/01_conversion/06_strconv/01_Atoi/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 )
 
 func main() {
 	var x = "15"
 	var y = 6
-	z, _ := strconv.Atoi(x)
+	z, err := strconv.Atoi(x)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	fmt.Println(y + z)
 }
