e8362311f84ed8989d8a9d09dce5ec0043d9ba28
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-19 06:27:47 +0600

    value receive pointer type

---
+++ 25_interface/03_empty_interface/05_method_sets/02_value_receiver_pointer_type/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct {
+	radius float64
+}
+type shape interface {
+	area() float64
+}
+
+func (c circle) area() float64 {
+	return (math.Pi * c.radius * c.radius)
+
+}
+func info(s shape) {
+	fmt.Println(s)
+	fmt.Println(s.area())
+}
+
+func main() {
+	c := circle{
+		radius: 5,
+	}
+	info(c)
+	info(&c)
+
+}
