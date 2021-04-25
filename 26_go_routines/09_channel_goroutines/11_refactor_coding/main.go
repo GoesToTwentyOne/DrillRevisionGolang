db5d5ad5764f066ccc8794d204cd883bc3145b5b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-25 06:08:04 +0600

    refactoring

---
+++ 26_go_routines/09_channel_goroutines/11_refactor_coding/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func increment() <-chan int {
+	chOut := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			chOut <- i
+		}
+		close(chOut)
+
+	}()
+	return chOut
+
+}
+func sumInc(s <-chan int) <-chan int {
+	chOut := make(chan int)
+	var sum int = 0
+	go func() {
+		for values := range s {
+			sum += values
+		}
+		chOut <- sum
+		close(chOut)
+
+	}()
+	return chOut
+
+}
+
+func main() {
+	chpass := increment()
+
+	for r := range sumInc(chpass) {
+		fmt.Println(r)
+	}
+
+}
+
+/*The optional <- operator specifies the channel direction, send or receive. If no direction is given,
+the channel is bidirectional. A channel may be constrained only to send or only to receive by assignment or
+explicit conversion.*/
