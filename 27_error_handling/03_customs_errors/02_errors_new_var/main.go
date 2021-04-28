3f7893b7564c0a77a355b16614765c5afbea9abc
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-04-28 06:48:29 +0600

    errors new var

---
+++ 27_error_handling/03_customs_errors/02_errors_new_var/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math"
+)
+
+var errorNew = errors.New("this is me")
+
+func main() {
+	fmt.Printf("%T \n", errorNew)
+	value, err := sqrt(-10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(value)
+
+}
+
+func sqrt(n float64) (float64, error) {
+	if n < 0 {
+		return 0, errorNew
+	}
+	return math.Sqrt(n), nil
+}
